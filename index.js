3b8c4373815f1a77db912b8360dea82f4c5e174a
Sowmitra Guha <sowmmochy@gmail.com>
2025-10-12 23:09:10 +0600

    upadate candidate profile

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",       // your local frontend
+    "https://yourfrontend.com"     // add production domain here later
+  ],
+  credentials: true,               // allow cookies / tokens
+}));
 app.use(express.json());
 
 // MongoDB connection
