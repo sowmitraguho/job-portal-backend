17aaa8d31b5483c1ab1e32b98a71b7c527e0784e
Sowmitra Guha <sowmmochy@gmail.com>
2025-10-12 06:08:22 +0600

    jobStatus

--- models/Job.js
+++ models/Job.js
@@ -17,7 +17,7 @@ const jobSchema = new mongoose.Schema({
   jobTitle: { type: String, required: true },
   companyName: { type: String, required: true },
   companyDescription: String,
-  jobStatus: { type: String, enum: ['Active', 'Expired', 'Cancelled'] },
+  status: { type: String, enum: ["Job Ongoing", "Candidate Listing", "Interviewing"] },
   companyGoals: String,
   location: String,
   jobDescription: String,
