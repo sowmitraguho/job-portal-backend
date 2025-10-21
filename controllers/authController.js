ca85ce0f096272cd906324f004842ce5818fd7b8
Sowmitra Guha <sowmmochy@gmail.com>
2025-10-22 01:09:18 +0600

    check user updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,7 +38,7 @@ export const loginUser = async (req, res) => {
       { expiresIn: '7d' }
     );
     //  Set cookie
-    res.cookie('auth-token', token, {
+    res.cookie('authToken', token, {
 
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
