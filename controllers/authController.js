4ac393a5be3c7f29ceb653b69692c887842fa4e7
Sowmitra Guha <sowmmochy@gmail.com>
2025-10-22 01:09:05 +0600

    check user updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,9 +39,7 @@ export const loginUser = async (req, res) => {
     );
     //  Set cookie
     res.cookie('auth-token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // only secure in production
-      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
