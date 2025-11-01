cb05b7fcb22a6c915c405dcffcc5ec899a23a869
Sowmitra Guha <sowmmochy@gmail.com>
2025-11-01 14:18:36 +0600

    some code edited

--- models/Review.js
+++ models/Review.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 
 const reviewSchema = new mongoose.Schema(
     {
-        userId: { type: mongoose.Schema.Types.ObjectId, required: true }, // can be Candidate or Employer
-        role: { type: String, enum: ['candidate', 'employer'] }, // specify the role
+        userId: { type: mongoose.Schema.Types.ObjectId, required: true }, 
+        role: { type: String }, 
         firstName: { type: String },
         lastName: { type: String },
         userName: { type: String },
         designation: { type: String },
-        email: { type: String, required: true }, // store email of reviewer
-        rating: { type: Number, required: true, min: 1, max: 5 },
+        email: { type: String }, // store email of reviewer
+        rating: { type: Number, min: 1, max: 5 },
         review: { type: String },
         image: { type: String },
         createdAt: { type: Date, default: Date.now },
